5db73b671d31f28da8c2ca3360aea02497f42077
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-08-11 15:51:10 -0400

    Updated general.js adding Promises to routers

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,38 +27,106 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.send(JSON.stringify(books, null, 4));
+public_users.get("/", async function (req, res) {
+  try {
+    const result = JSON.stringify(books, null, 4);
+    return res.send(await result);
+  } catch (err) {
+    res.status(404).send("Error getting books list: " + err);
+  }
 });
+
+// Create new promise function to solve Get Books By ISBN
+const getBooksByISBN = (isbn) => {
+  return new Promise((resolve, reject) => {
+    // Only resolve if ISBN is within range or of DB
+    if (isbn > 0 && isbn <= 10) {
+      resolve(books[isbn]);
+    } else {
+      reject("Please type in a valid ISBN");
+    }
+  });
+};
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
+  // Calling Promise to handdle the user request using ISBN as parameter
   const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+  getBooksByISBN(isbn)
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((e) => {
+      return res.status(404).send(e);
+    });
 });
 
-// Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  const author = req.params.author;
-  const booksArray = Object.values(books);
-  let booksByAuthorArray = [];
-  booksArray.forEach((book) => {
-    if (book.author === author) booksByAuthorArray.push(book);
+// Create new promise function to solve Get book details By author
+const getBooksByAuthor = (author) => {
+  return new Promise((resolve, reject) => {
+    // Get array from books values
+    const booksArray = Object.values(books);
+    // Only resolve if author exists in DB
+    const authorExists = booksArray.find((i) => i.author === author);
+    if (authorExists) {
+      let booksByAuthorArray = [];
+      // Push to new array only values that match with author
+      booksArray.forEach((book) => {
+        if (book.author === author) booksByAuthorArray.push(book);
+      });
+      // Give result shape of an object with request title as property
+      const booksByAuthor = { booksByAuthor: booksByAuthorArray };
+      resolve(booksByAuthor);
+    } else {
+      reject("Author not in DB");
+    }
   });
-  const booksByAuthor = { booksByAuthor: booksByAuthorArray };
-  return res.send(booksByAuthor);
+};
+
+// Get book details based on author
+public_users.get("/author/:author", async function (req, res) {
+  const author = req.params.author;
+  getBooksByAuthor(author)
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((e) => {
+      return res.status(404).send(e);
+    });
 });
+
+// Create new promise function to solve Get book details By title
+const getBooksByTitle = (title) => {
+  return new Promise((resolve, reject) => {
+    // Get array from books values
+    const booksArray = Object.values(books);
+    // Only resolve if title exists in DB
+    const titleExists = booksArray.find((i) => i.title === title);
+    if (titleExists) {
+      let booksByTitleArray = [];
+      // Push to new array only values that match with title
+      booksArray.forEach((book) => {
+        if (book.title === title) booksByTitleArray.push(book);
+      });
+      // Give result shape of an object with request title as property
+      const booksByTitle = { booksByTitle: booksByTitleArray };
+      resolve(booksByTitle);
+    } else {
+      reject("Title not in DB");
+    }
+  });
+};
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   const title = req.params.title;
-  const booksArray = Object.values(books);
-  let bookByTittleArray = [];
-  booksArray.forEach((book) => {
-    if (book.title === title) bookByTittleArray.push(book);
-  });
-  const booksByTitle = { booksByTitle: bookByTittleArray };
-  return res.send(booksByTitle);
+  getBooksByTitle(title)
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((e) => {
+      return res.status(404).send(e);
+    });
 });
 
 //  Get book review
